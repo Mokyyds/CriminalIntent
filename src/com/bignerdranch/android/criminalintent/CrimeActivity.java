3ead4bbecf61b83490ea6403ab223e3404f30a90
Toalei.Zhou <568797992@qq.com>
2015-11-25 15:46:55 +0800

    Enter a title for this crime.

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,24 +14,15 @@ public class CrimeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+        
+        FragmentManager fm = getFragmentManager();
+        Fragment fragement = fm.findFragmentById(R.id.fragmentContainer);
+        
+        if (fragement == null) {
+			fragement = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragement).commit();
+			
+		}
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }

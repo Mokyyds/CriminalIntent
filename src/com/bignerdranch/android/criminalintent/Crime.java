c8b277270bc926e1c1b33bfcd8885b485899ee33
Toalei.Zhou <568797992@qq.com>
2015-11-25 18:44:10 +0800

    Weight learn

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,18 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
 	public Crime(){
 		//生成唯一标识符
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public UUID getmId() {
@@ -21,6 +25,22 @@ public class Crime {
 
 	public void setmTitle(String mTitle) {
 		this.mTitle = mTitle;
+	}
+
+	public Date getmDate() {
+		return mDate;
+	}
+
+	public void setmDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public boolean ismSolved() {
+		return mSolved;
+	}
+
+	public void setmSolved(boolean mSolved) {
+		this.mSolved = mSolved;
 	}
 
 }
